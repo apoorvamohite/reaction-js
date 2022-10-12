2beca6070096eb626e4a4cf600d181bd8d6940a4
Apoorva Mohite <apps.mohite@gmail.com>
2022-10-11 19:30:19 -0700

    Authentication page styling

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import { getAuth,
          signInWithRedirect, 
          signInWithPopup, 
          GoogleAuthProvider, 
-         createUserWithEmailAndPassword } from "firebase/auth";
+         createUserWithEmailAndPassword, 
+         signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -51,4 +52,9 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
     if(!email || !password) return;
     return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+    return await signInWithEmailAndPassword(auth, email, password);
 }
